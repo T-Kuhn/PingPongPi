6e3e65233795139b03f4f66a525d490b0eed2580
Tobias Kuhn <tkuhn.mpu@gmail.com>
2016-10-03 03:02:11 +0000

    initial commit

---
+++ PingPongPi.py
@@ -0,0 +1,97 @@
+import io
+import time
+import threading
+import picamera
+from PIL import Image
+
+# - - - - - - - - - - - - - - - - - -
+# Create a pool of image processors
+# - - - - - - - - - - - - - - - - - -
+done = False
+lock = threading.Lock()
+pool = []
+
+# - - - - - - - - - - - - - - - - - -
+# - - - Image Processor Class - - - - 
+# - - - - - - - - - - - - - - - - - -
+class ImageProcessor(threading.Thread):
+    def __init__(self):
+        super(ImageProcessor, self).__init__()
+        self.stream = io.BytesIO()
+        self.event = threading.Event()
+        self.terminated = False
+        self.start()
+
+    def run(self):
+        # This method runs in a separate thread
+        global done
+        counterThing = 0
+        while not self.terminated:
+            # Wait for an image to be written to the stream
+            if self.event.wait(1):
+                try:
+                    self.stream.seek(0)
+                    # Read the image and do some processing on it
+                    img = Image.open(self.stream)
+                    
+                    counterThing += 1
+                    
+                    if counterThing < 10:
+                        print ("taking an image")
+                    else:
+                        img.save("out.bmp")
+                        done = True
+                    #...
+                    #...
+                    # Set done to True if you want the script to terminate
+                    # at some point
+                    #done=True
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the pool
+                    with lock:
+                        pool.append(self)
+
+# - - - - - - - - - - - - - - - - - -
+# - - - - Streams Function  - - - - - 
+# - - - - - - - - - - - - - - - - - -
+def streams():
+    while not done:
+        with lock:
+            if pool:
+                processor = pool.pop()
+            else:
+                processor = None
+        if processor:
+            yield processor.stream
+            processor.event.set()
+        else:
+            # When the pool is starved, wait a while for it to refill
+            time.sleep(0.02)
+
+# - - - - - - - - - - - - - - - - - -
+# - - - - - Main Program  - - - - - - 
+# - - - - - - - - - - - - - - - - - -
+with picamera.PiCamera() as camera:
+    pool = [ImageProcessor() for i in range(4)]
+    camera.resolution = (128, 128)
+    camera.framerate = 90
+    time.sleep(2)
+    # Now fix the values
+    camera.shutter_speed = 2720 #int(camera.exposure_speed/4)
+    print(str(camera.shutter_speed))
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
+    camera.capture_sequence(streams(), use_video_port=True)
+
+# Shut down the processors in an orderly fashion
+while pool:
+    with lock:
+        processor = pool.pop()
+    processor.terminated = True
+    processor.join()
